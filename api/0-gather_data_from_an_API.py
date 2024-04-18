73cea476511ef382be144c2f3c525d539cabb2dc
jordiindahood <hazem.ouni123@gmail.com>
2024-04-18 16:35:54 +0100

    script 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""
+This script will use the REST API to return information
+about a given employee's TODO list progress.
+"""
 import requests
 import sys
 
 
 def TODO_REQUESTS(ID):
+    """
+    This functionis the same as the previous one but with a different
+    way to get the data using requests
+    """
     todos = requests.get(
         f"https://jsonplaceholder.typicode.com/todos?userId={ID}"
     ).json()
@@ -22,4 +30,5 @@ typicode.com/users/{ID}"
 
 if __name__ == "__main__":
     ID = sys.argv[1]
+
     TODO_REQUESTS(ID)
