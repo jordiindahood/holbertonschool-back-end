3fef624ebea002e890da7bf8c0a223d97ef15529
jordiindahood <hazem.ouni123@gmail.com>
2024-04-18 17:41:45 +0100

    script 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script will use the REST API to return information
-about a given employee's TODO list progress.
+a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
 import requests
 import sys
@@ -9,19 +9,19 @@ import sys
 
 def TODO_REQUESTS(ID):
     """
-    This functionis the same as the previous one but with a different
-    way to get the data using requests
+    This function gets the date from jsonplaceholder.typicode.com
+    and shows it
     """
     todos = requests.get(
         f"https://jsonplaceholder.typicode.com/todos?userId={ID}"
     ).json()
-    user_info = requests.get(
+    user = requests.get(
         f"https://jsonplaceholder.\
 typicode.com/users/{ID}"
     ).json()
     completed_tasks = [task["title"] for task in todos if task["completed"]]
     print(
-        f"Employee {user_info['name']} is done with tasks\
+        f"Employee {user['name']} is done with tasks\
 ({len(completed_tasks)}/{len(todos)}):"
     )
     for task in completed_tasks:
