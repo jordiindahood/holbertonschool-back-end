7c1deb627c8c3369648fdc9b44ed0237fbc84e24
jordiindahood <hazem.ouni123@gmail.com>
2024-04-18 16:05:47 +0100

    script 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -15,12 +15,13 @@ def GET_DATA_BY_ID(ID):
         f"https://jsonplaceholder.typicode.com/todos?userId={ID}"
     ).json()
     user_info = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{ID}"
+        f"https://jsonplaceholder.\
+typicode.com/users/{ID}"
     ).json()
-
     completed_tasks = [task["title"] for task in todos if task["completed"]]
     print(
-        f"Employee {user_info['name']} is done with tasks ({len(completed_tasks)}/{len(todos)}):"
+        f"Employee {user_info['name']} is done with tasks\
+({len(completed_tasks)}/{len(todos)}):"
     )
     for task in completed_tasks:
         print(f"\t {task}")
